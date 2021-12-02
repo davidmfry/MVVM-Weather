03c670e29e21bb85e164d554610eca9d50c1c25b
davidmfry <david.fry.tv@gmail.com>
2021-12-01 20:00:00 -0800

    init commit

---
+++ MVVMEather/Models.swift
@@ -0,0 +1,11 @@
+//
+//  Models.swift
+//  MVVMEather
+//
+//  Created by David Fry on 12/1/21.
+//
+
+
+import Foundation
+
+// Data
---
+++ MVVMEather/ViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ViewModel.swift
+//  MVVMEather
+//
+//  Created by David Fry on 12/1/21.
+//
+
+import Foundation
+
+// Data needed by the view
