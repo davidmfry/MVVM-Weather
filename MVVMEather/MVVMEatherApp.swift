653598e232d369836bfed213d03886a881e304ea
davidmfry <david.fry.tv@gmail.com>
2021-12-01 19:57:37 -0800

    Initial Commit

---
+++ MVVMEather/MVVMEatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMEatherApp.swift
+//  MVVMEather
+//
+//  Created by David Fry on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVMEatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
